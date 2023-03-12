4539b9d7bc216310f9ea05d1b22317742a0d56cf
irmantasdv <53112192+irmantasdv@users.noreply.github.com>
2023-03-12 18:04:10 +0200

    Interface for csv, xml, json file readers

--- CreationProject/Infrastructure/Adapter/FileReader.php
+++ CreationProject/Infrastructure/Adapter/FileReader.php
@@ -2,7 +2,15 @@
 
 namespace CreationProject\Infrastructure\Adapter;
 
+/**
+ * Interface FileReader
+ */
 interface FileReader
 {
-
+    /**
+     * @param string $path
+     * @param string $fileName
+     * @return array
+     */
+    public function readFromFile(string $path, string $fileName) : array;
 }
