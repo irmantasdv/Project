33354e9b44399f56dd42a937625f29ccc54a8a92
irmantasdv <53112192+irmantasdv@users.noreply.github.com>
2023-03-13 01:12:21 +0200

    Factory can create FileReader using .env config file

--- CreationProject/Infrastructure/Adapter/FileReaderFactory.php
+++ CreationProject/Infrastructure/Adapter/FileReaderFactory.php
@@ -3,14 +3,37 @@
 namespace CreationProject\Infrastructure\Adapter;
 
 use CreationProject\Infrastructure\Repository\UserDataFromFileRepositoryImplementation;
+use EnvLoader;
+use Exception;
 
 class FileReaderFactory
 {
-    static function create($fileName): UserDataFromFileRepositoryImplementation
+    /**
+     * @throws Exception
+     */
+    static function create($file): UserDataFromFileRepositoryImplementation
     {
-        // gets file type, PATHINFO_EXTENSION flag returns value after dot.
+        $envLoader = new EnvLoader();
+        $availableFileTypeArray = $envLoader->getEnv();
+
+        // gets file name
+        $fileName = $file['name'];
+        // gets file path
+        $pathName = $file['tmp_name'];
+        // gets file type, PATH INFO_EXTENSION flag returns value after dot.
         $fileType = pathinfo($fileName, PATHINFO_EXTENSION);
-        return new UserDataFromFileRepositoryImplementation($fileName,$fileType);
+        // checks if file type exists in .env file
+        if (!$availableFileTypeArray[$fileType]) {
+            throw new Exception("File type not valid");
+        }
+        // creates class name (string) from file type
+        $fileClass = $availableFileTypeArray[$fileType];
+        // creates namespaces for class
+        $class = "CreationProject\Infrastructure\Adapter" . "\\" . $fileClass;
+        // creates exact FileReader class
+        $reader = new $class;
+
+        return new UserDataFromFileRepositoryImplementation($pathName, $fileName, $reader);
     }
 
 }
