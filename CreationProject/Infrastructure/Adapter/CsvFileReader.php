c378506d7863a0ae1d97b6e03635ea08b93f60c4
irmantasdv <53112192+irmantasdv@users.noreply.github.com>
2023-03-13 05:01:10 +0200

    return array from csv file

--- CreationProject/Infrastructure/Adapter/CsvFileReader.php
+++ CreationProject/Infrastructure/Adapter/CsvFileReader.php
@@ -1,12 +1,34 @@
 <?php
+
 namespace CreationProject\Infrastructure\Adapter;
 
+/**
+ * Class CsvFileReader
+ */
 class CsvFileReader implements FileReader
 {
 
+    /**
+     * @param string $path
+     * @param string $fileName
+     * @return array
+     */
     public function readFromFile(string $path, string $fileName): array
     {
-        $fileData = file_get_contents($path, $fileName);
-        return json_decode($fileData,true);
+//         works only with files without quotation
+        $allRows = array();
+        $header = null;
+        if (($file = fopen($path, "r")) !== FALSE) {
+            while (($row = fgetcsv($file, 1000, ",")) !== FALSE) {
+                if ($header === null) {
+                    $header = $row;
+                    continue;
+                }
+                $allRows[] = array_combine($header, $row);
+            }
+            fclose($file);
+        }
+        return $allRows;
+
     }
 }
