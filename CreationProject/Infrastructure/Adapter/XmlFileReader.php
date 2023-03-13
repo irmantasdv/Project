afdf72279dd69d0d0f386a7f128afe2417a3be15
irmantasdv <53112192+irmantasdv@users.noreply.github.com>
2023-03-13 05:02:26 +0200

    Return array from Xml file

--- CreationProject/Infrastructure/Adapter/XmlFileReader.php
+++ CreationProject/Infrastructure/Adapter/XmlFileReader.php
@@ -1,8 +1,29 @@
 <?php
+
 namespace CreationProject\Infrastructure\Adapter;
 
-class XmlFileReader
+/**
+ * Class XmlFileReader
+ */
+class XmlFileReader implements FileReader
 {
 
-
+    /**
+     * @param string $path
+     * @param string $fileName
+     * @return array
+     */
+    public function readFromFile(string $path, string $fileName): array
+    {
+        $data = array();
+        $xmlFileContent = simplexml_load_file($path);
+        foreach ($xmlFileContent->children() as $child) {
+            $arrayAssoc = array();
+            foreach ($child->children() as $grandChild) {
+                $arrayAssoc[$grandChild->getName()] = (string)$grandChild;
+            }
+            $data[] = $arrayAssoc;
+        }
+        return $data;
+    }
 }
